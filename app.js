388ae602c5b58a041b7090ae5a7b5c729a6e5b8e
FixTheInternet <vmpatoleia@protonmail.com>
2018-02-10 17:05:14 +0000

    Fetching weather from Forecast.io

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+const request = require('request')
 const yargs = require('yargs');
 const geocode = require('./geocode/geocode')
 
@@ -23,4 +24,18 @@ geocode.geocodeAddress(argv.address, (erroMessage, results) => {
   } else {
     console.log(JSON.stringify(results, undefined, 2 ));
   }
+})
+
+// 551e6f192413f7d237d0ce9b824a0491
+
+request({
+  url: 'https://api.darksky.net/forecast/551e6f192413f7d237d0ce9b824a0491/38.7222524,-9.1393366',
+  json: true
+}, (error, response, body) => {
+  if(!error && response.statusCode === 200) {
+    console.log(body.currently.temperature);
+  } else {
+    console.log('Unable to fetch Weather.');
+  }
+
 })
