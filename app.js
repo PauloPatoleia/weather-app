65b181345f0d8962d337436c4f8f09f498262d42
FixTheInternet <vmpatoleia@protonmail.com>
2018-02-10 09:52:52 +0000

    Error handling

--- app.js
+++ app.js
@@ -20,7 +20,14 @@ request({
   url: `https://maps.googleapis.com/maps/api/geocode/json?address=${encodeURIComponent(argv.address)}`,
   json: true
 }, (error, response, body) => {
-  console.log(`Adress: ${body.results[0].formatted_address}`);
-  console.log(`Lat: ${body.results[0].geometry.location.lat}`);
-  console.log(`Lng: ${body.results[0].geometry.location.lng}`);
+  if(error) {
+    console.log('unable to connect to Google servers.');
+  } else if (body.status === 'ZERO_RESULTS') {
+    console.log('Address not found.');
+  } else if (body.status === 'OK') {
+    console.log(`Adress: ${body.results[0].formatted_address}`);
+    console.log(`Lat: ${body.results[0].geometry.location.lat}`);
+    console.log(`Lng: ${body.results[0].geometry.location.lng}`);
+  }
+
 })
